de43f56b392e4ead81e0494d124f469f65420057
Tamoziit <tamojitdas181007@gmail.com>
2024-08-17 22:41:23 +0530

    date-time pid problem

---
+++ p2.sh
@@ -0,0 +1,28 @@
+echo "Please enter your name:"
+read USER_NAME
+
+# Get the current hour in 24-hour format
+HOUR=$(date +%H)
+
+# Determine the appropriate greeting based on the time of day
+if [ $HOUR -ge 0 ] && [ $HOUR -lt 12 ] 
+	then
+    	GREETING="Good Morning"
+elif [ $HOUR -ge 12 ] && [ $HOUR -lt 18 ]
+	then
+	    GREETING="Good Afternoon"
+else
+    GREETING="Good Evening"
+fi
+
+# Get the current date and time
+CURRENT_DATE_TIME=$(date)
+
+# Get the process ID of the terminal
+TERMINAL_PID=$$
+
+# Print the greeting along with user's name, current date & time, and terminal process ID
+echo "$GREETING, $USER_NAME!"
+echo "Current date and time: $CURRENT_DATE_TIME"
+echo "Your terminal process ID: $TERMINAL_PID"
+
